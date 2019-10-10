6836cd03fd4be38685ee53d03f50b3d881379723
3g7 <site.3g7@gmail.com>
2019-10-10 11:29:24 +0800

    [update] 改变了异常信息文件的读取方式，修改了一些注释错误
    Signed-off-by: 3g7 <site.3g7@gmail.com>

---
+++ tummy-base-core/src/main/java/com/fayelau/tummy/base/core/utils/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.fayelau.tummy.base.core.utils;
+
+public class CommonConstants {
+    
+    public static final String DIRECTION_ASC = "asc";
+    public static final String DIRECTION_DESC = "desc";
+
+}
